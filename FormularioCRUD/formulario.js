a8203078d77409f92185411da8406eea16c6de11
Franchesco <franciscocastano.pdf@gmail.com>
2025-11-19 14:07:02 +0100

    Prueba

--- FormularioCRUD/formulario.js
+++ FormularioCRUD/formulario.js
@@ -3,37 +3,53 @@ window.addEventListener('load', () => {
 
     let formulario = document.getElementById("formulario");
 
-    let nombre = document.getElementById("nombre");
+    let nombre = document.getElementById("nombre").value;
 
-    let apellidos = document.getElementById("apellidos");
+    let apellidos = document.getElementById("apellidos").value;
 
-    let dni = document.getElementById("dni");
+    let cdni = document.getElementById("dni");
+
+    let dni = cdni.value;
  
-    let email = document.getElementById("email");
+    let email = document.getElementById("email").value;
     
 
-    function validarFormulario() {
+    let enviar = document.getElementById("enviar");
 
-            if (condition) {
-                
-            }
+    
+    cdni.addEventListener("change", ()=> {
+
+        verificarDNI(dni);
+
+    });
+
+   
+
+    enviar.addEventListener('click', function(event) {
+
+        event.preventDefault();
+        validarFormulario();
+
+    });
+
+    function validarFormulario() {                
+            
 
     }   
 
-    function validarNombre() {
 
-        if (!(nombre.value.length < 2 && nombre.value.lengt > 10)) {
-            
-            let errorNombre = errorNombre.textcontent = "El nombre no puede ser menor que dos caracteres, ni mayor que 10.";
-
-        }
-
+    
+    //FUNCION PARA VALIDAR EL DNI:
+   function verificarDNI(dni) {
+    
+    if (dni.length > 0 && dni.length < 9) {
+        console.log("TodoJoya");
+    }else {
+        alert("Debes introducir un Dni valido")
     }
 
-    nombre.addEventListener(validarNombre);
 
-
-
+}
 
 
 
