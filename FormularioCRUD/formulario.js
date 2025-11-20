302e9c758895deeb7ac2d083ec0d2a38bf931c33
Franchesco <franciscocastano.pdf@gmail.com>
2025-11-20 13:04:29 +0100

    FUNCIONA EL FETCH

--- FormularioCRUD/formulario.js
+++ FormularioCRUD/formulario.js
@@ -3,54 +3,67 @@ window.addEventListener('load', () => {
 
     let formulario = document.getElementById("formulario");
 
-    let nombre = document.getElementById("nombre").value;
-
-    let apellidos = document.getElementById("apellidos").value;
-
-    let cdni = document.getElementById("dni");
-
-    let dni = cdni.value;
- 
-    let email = document.getElementById("email").value;
-    
-
-    let enviar = document.getElementById("enviar");
-
-    
-    cdni.addEventListener("change", ()=> {
-
-        verificarDNI(dni);
-
-    });
-
    
 
-    enviar.addEventListener('click', function(event) {
+    formulario.addEventListener('submit', function(event) {
 
         event.preventDefault();
-        validarFormulario();
-
+        validarFormulario(); 
+    
     });
+    
 
     function validarFormulario() {                
             
+        let nombre = document.getElementById("nombre").value.trim();
+
+        let apellidos = document.getElementById("apellidos").value.trim();
+
+        let cdni = document.getElementById("dni");
+
+        let dni = cdni.value.trim().toUpperCase();
+ 
+        let email = document.getElementById("email").value.trim();
+
+        let usu = {
+
+            nombre: nombre,
+            apellidos: apellidos,
+            dni: dni,
+            email: email
+
+        };
+
+        guardarUsuario(usu);
 
     }   
 
+    function guardarUsuario(usu) {
 
-    
-    //FUNCION PARA VALIDAR EL DNI:
-   function verificarDNI(dni) {
-    
-    if (dni.length > 0 && dni.length < 9) {
-        console.log("TodoJoya");
-    }else {
-        alert("Debes introducir un Dni valido")
-    }
+        fetch("http://localhost:3000/usuarios", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(usu)
+        })
+        .then(res => {
 
+            return res.json();
 
-}
+        })
+        .then(data => {
 
+            alert("Uusario guardao");
+
+        })
+        .catch(err => {
+
+            console.error("Que haces bobo", err)
+
+        });
+
+     }
 
 
 });
