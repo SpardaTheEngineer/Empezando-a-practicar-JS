1eac47c7da04826d8be3b546c9c413d6067373da
Franchesco <franciscocastano.pdf@gmail.com>
2025-12-08 15:07:36 +0100

    M

--- 2EV/bar/main.js
+++ 2EV/bar/main.js
@@ -9,7 +9,7 @@ window.addEventListener("load", () =>  {
 
     let botonAñadirMesa = document.createElement("img");
 
-    botonAñadirMesa.src = "/bar/asserts/BotonAñadir.png";
+    botonAñadirMesa.src = "/asserts/BotonAñadir.png";
     botonAñadirMesa.style.width = "30px";
 
     botonAñadir.appendChild(botonAñadirMesa);
@@ -18,44 +18,106 @@ window.addEventListener("load", () =>  {
 
 //----------------------Añadir la mesa----------------------------------
 
-    let mesa = document.createElement("img");
+    let modal = document.createElement("dialog");
+
+    modal.innerHTML = `
+    
+    <h2>Indica el número de comensales</h2>
+
+    <input 
+        type="number" 
+        id="numComensales" 
+        placeholder="Indica el numero aqui..."
+        min="1"
+    >
+
+    <br><br>
+
+    <button id="btnAceptar">Aceptar</button>
+    <button id="btnCancelar">Cancelar</button>
+
+    `;
+
+    let ventanaModal = document.getElementById("ventanaModal");
+
+    ventanaModal.appendChild(modal);
 
     let pinchada = false;
 
-    mesa.src = "/bar/asserts/mesa.png";
-    mesa.style.width = "70px";
-    mesa.classList.add("mesa");
-    
-    botonAñadir.addEventListener("mousedown", (e) => {
+    let x;
+    let y;
 
-        if (e.button==2){
-            pinchada = true;
+    //EMPEZAR
+    botonAñadir.addEventListener("click", (e) => {
+     
+        pinchada = true;
+        
+        modal.showModal();
+
+        //creamos la mesa
+        let mesa = document.createElement("img");
+        mesa.classList.add("mesa");
+
+        //BOTON ACEPTARRRRR
+        let btnAceptar = document.getElementById("btnAceptar");
+
+        btnAceptar.addEventListener("click", (e) => {
+
+            //el numero de comens
+            let personas = document.getElementById("numComensales").value.trim();
+
+            console.log(personas);
+            if (personas <= 2) {
+            
+            mesa.src = "/asserts/mesa2.png";
+
+            }else if (personas <= 4 && personas > 2) {
+            
+            mesa.src = "/asserts/mesa4.png";
+
+            }else {
+
+                alert("Sois demasiados llama a la camarera y juntar dos mesas");
+
+            }
+
+            modal.close();
+
+        //pintamos la mesa
+        ponerMesa.appendChild(mesa);
+
         });
 
-        ponerMesa.appendChild(mesa);
+
+        //Comprobamos el numero de comens e imponemos una imagen u otra
+        
+
+    });
+
 
     //----------------------Mover la mesa----------------------------------
 
-    if (pinchada) {
-        
+        mesa.addEventListener("mosuedown", (e) => {
+
+            if (e.button === 2) {
+                
+                pinchada = true;
+                x = e.clientX - mesa.offsetLeft;
+                y = e.clientY - mesa.offsetRight;
+            }
+
+        });
+
+        mesa.addEventListener("mosuemove", (e) => {
+
+            
+
+
+
+        });
     
-        mesa.addEventListener("mousemove", (e) => {
 
-            mesa.style.top = e.clientY + "px";
-            mesa.style.left = e.clientX + "px";
-
-        });
-
-    }else {
-
-        mesa.addEventListener("mouseout", (e) => {
-
-
-
-        });
-
-
-    }
+ 
 
     //----------------------------------------------------------------------
 
@@ -74,4 +136,4 @@ window.addEventListener("load", () =>  {
 
 
 
-});
+ });
